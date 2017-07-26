ane/rats.el

commit 49b9166bf5cc01e47047346481fd969f376bf1de

Use compilation-mode in *rats-test* buffers
This enables user to jump directly to the lines of code mentioned in
the traceback.

rats.el
@@ -120,7 +120,8 @@
     (if go-command
         (let* ((output-buffer-name "*rats-test*") 
                (arguments '("test" "-v"))
-               (full-args (append arguments )))
+               (full-args (append arguments))
+               (inhibit-read-only t))
           (when (get-buffer output-buffer-name)
             (with-current-buffer (get-buffer output-buffer-name)
               (erase-buffer)))
@@ -134,7 +135,8 @@
                 (if (s-present? test)
                     (or (rats--get-test-results (buffer-string) test)
                         `((err . ,(format "The test %s was not run." test))))  
-                  (rats--parse-results (buffer-string)))))))
+                  (rats--parse-results (buffer-string))))
+              (compilation-mode))))
       `((err . ,(format "`%s' command not found in PATH!" rats-go-executable-name))))))
 
 (defun rats--report-result (result &optional name)

commit a2cb5bae09745dc5b2c10f28a96f0070e63b8e83

Set compilation-mode before returning results
Otherwise, rats--run-go-test would return nil, and we would
erroneously report that the test run errored.

Broken in 49b9166bf5cc01e47047346481fd969f376bf1de.

rats.el
@@ -130,13 +130,13 @@
                           (if (s-present? test) "-run" "")
                           (if (s-present? test) test ""))
             (with-current-buffer output-buffer
+              (compilation-mode)
               (if (rats--failed-p (buffer-string))
                   `((err . ,(rats--format-failure (buffer-string))))
                 (if (s-present? test)
                     (or (rats--get-test-results (buffer-string) test)
                         `((err . ,(format "The test %s was not run." test))))  
-                  (rats--parse-results (buffer-string))))
-              (compilation-mode))))
+                  (rats--parse-results (buffer-string)))))))
       `((err . ,(format "`%s' command not found in PATH!" rats-go-executable-name))))))
 
 (defun rats--report-result (result &optional name)